8fdd219f193a278a28d51d05ceb13686462307cc
Thanh Minh <vmthanh24@gmail.com>
2021-03-29 17:21:40 +0700

    Chapter 1: 20 most useful powershell commands

--- PSAdmin.ps1
+++ PSAdmin.ps1
@@ -29,3 +29,39 @@ Get-Process
 Exit-PSSession
 Get-Process -Id 17912 | Stop-Process
 
+# Manage Event logs
+Get-Command -Name *Event*
+# Get-EventLog
+Get-WinEvent -LogName System -MaxEvents 5
+$logs = Get-WinEvent -LogName Application -MaxEvents 5
+# Clear EventLog
+foreach($log in $logs){ wevtutil.exe cl $log.LogName }
+
+# Get computer information
+$report = "C:\Temp\Data\reports.log"
+$physicalMemory = Get-CimInstance -ClassName Win32_PhysicalMemory
+$processor = Get-CimInstance -ClassName Win32_Processor
+$logicalDisk = Get-CimInstance -ClassName Win32_LogicalDisk
+$diskDrive = Get-CimInstance -ClassName Win32_DiskDrive
+$operatingSystem = Get-CimInstance -ClassName Win32_OperatingSystem
+
+New-Item -Path $report -ItemType File -Value "Computer Information"
+Add-Content $report "************Physical Memory************"
+Add-Content $report $physicalMemory
+Add-Content $report "*************Processor******************"
+Add-Content $report $processor
+Add-Content $report "**************Logical Disk***************"
+Add-Content $report $logicalDisk
+Add-Content $report "************Disk Drive****************"
+Add-Content $report $diskDrive
+Add-Content $report "*************Operating System*************"
+Add-Content $report $operatingSystem
+
+# Powershell remoting
+Enter-PSSession localhost
+Exit-PSSession
+# Execute command on remote computer
+Invoke-Command -ComputerName localhost -ScriptBlock { Get-Process }
+$session = New-PSSession -ComputerName localhost
+# $Processes = Invoke-Command -Session $session { Get-Process }
+foreach($process in $Processes) { Write-Output $process.Name }
